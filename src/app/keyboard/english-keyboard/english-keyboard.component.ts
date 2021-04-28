5d884dd7d8cd801eacdf57f5688c1c63ae1301af
Elyasin Shaladi <Elyas.Shaladi@gmail.com>
2021-04-28 05:36:19 +0200

    Implements font class for keyboards

--- src/app/keyboard/english-keyboard/english-keyboard.component.ts
+++ src/app/keyboard/english-keyboard/english-keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-english-keyboard',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnglishKeyboardComponent implements OnInit {
 
-  constructor() { }
+  private fontClass = 'alphonetik-gb';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  getFontClass(): string {
+    return this.fontClass;
   }
 
 }
--- src/app/keyboard/french-keyboard/french-keyboard.component.ts
+++ src/app/keyboard/french-keyboard/french-keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-french-keyboard',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrenchKeyboardComponent implements OnInit {
 
-  constructor() { }
+  private fontClass = 'alphonetik';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  getFontClass(): string {
+    return this.fontClass;
   }
 
 }
